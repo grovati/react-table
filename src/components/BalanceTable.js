1098df1cc1493ad3d7d7001fcc917d13b3e50885
german Rovati <germanrovati@Germans-MacBook-Pro-3.local>
2020-04-18 18:42:04 -0400

    Add styled component to table

--- src/components/BalanceTable.js
+++ src/components/BalanceTable.js
@@ -3,13 +3,26 @@ import styled from 'styled-components';
 import Row from './Row';
 
 const Styled = styled('div')`
+  padding: 10px 20px 0 px 20px;
+
+  table {
+    border: 1px solid black;
+    border-collapse: collapse;
+
+    th,
+    td {
+      padding: 5px;
+      border: 1px solid black;
+      text-align: center;
+    }
+  }
 `;
 
 //Use columns and data to create the UI
 const BalanceTable = ({columns, data}) => {
 
   return (
-    <>
+    <Styled>
       <table>
         <thead>
           <Row content={columns} isHeader={true}/>
@@ -18,7 +31,7 @@ const BalanceTable = ({columns, data}) => {
           <Row content={data} isHeader={false}/>
         </tbody>
       </table>
-    </>
+    </Styled>
   );
 };
 
